474244310839652fdb12305aee50532a9e3895c4
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-12 07:58:16 -0500

    some numpy and pandas 🐼 stuff

--- basics.py
+++ basics.py
@@ -1,6 +1,8 @@
 import pjoe.joebar as pj
 import mygame.draw as md
 import mygame.game as mg
+import numpy as np
+import pandas as pd
 
 mylist = []
 
@@ -109,3 +111,31 @@ play_state = mg.play_game()
 md.draw_game(play_state)
 
 pj.joe()
+
+height = [1.87, 1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+bmi = np_weight / np_height**2
+
+print(bmi)
+print(bmi > 23)
+print(bmi[bmi > 25])
+
+dict = {
+    "country": ["Brazil", "Russia", "India", "China", "South Africa"],
+    "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+    "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+    "population": [200.4, 143.5, 1252, 1357, 52.98],
+}
+
+brics = pd.DataFrame(dict)
+print(brics)
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+print(brics)
+
+cars = pd.read_csv("cars.csv", index_col=0)
+print(cars)
+print(cars.sort_values(by="awesomeness", ascending=False))
