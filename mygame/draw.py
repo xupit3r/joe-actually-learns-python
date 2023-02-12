2a16c5449d93420de6877e4af0669cb5220e096d
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-12 06:48:03 -0500

    some module basics, bruh

---
+++ mygame/draw.py
@@ -0,0 +1,6 @@
+def draw_game(play_state):
+    pass
+
+
+def clear_screen():
+    pass
---
+++ mygame/game.py
@@ -0,0 +1,14 @@
+import draw
+
+
+def play_game():
+    pass
+
+
+def main():
+    result = play_game()
+    draw.draw_gam(result)
+
+
+if __name__ == "__main__":
+    main()
